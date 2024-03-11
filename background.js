57bf65071fb0aab98c27fa319fcff8daf2763be6
poyi <inmywing@hotmail.com>
2024-03-11 15:15:53 +0900

    modify extractVideoId

--- background.js
+++ background.js
@@ -3,13 +3,10 @@ let startup_youtube_tabs_url;
 const forbiddenKeywords = ["youtube", "netflix", "disney"];
 
 const extractVideoId = (url) => {
-    // Define a regular expression to match YouTube video URLs
-    const regex = /(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const regex = /watch\?v=(.{11})/;
   
-    // Use the regular expression to extract the video ID
     const match = url.match(regex);
-  
-    // Check if a match is found and return the video ID
+
     return match ? match[1] : null;
 }
 
